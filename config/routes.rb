007ca90ac688b974793e725a17de5e2fe8b65229
Tygh Walters <tyghwalters@gmail.com>
2023-01-21 16:01:32 -0800

    Make Edit User Flow

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,3 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  post 'users/create'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :edit, :update]
 end
